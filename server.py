6c37f1fd55e135bbb9b3c1d4691a392fea3d4fcf
kkz40401637 <kaungkhantzin123@gmail.com>
2018-04-06 08:23:52 +0630

    Final

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 w=json.load(open("worldl.json"))
@@ -17,16 +17,23 @@ def mainPage():
         page_number=0
         )     
                            
-
+@app.route('/begin/<b>')
+def beginPage(b):
+	bn = int(b)
+	return render_template('index.html',
+		w = w[bn:bn+page_size],
+		page_number = bn,
+		page_size = page_size
+		)
+    
 @app.route('/continent/<a>')
 def continentPage(a):
-    cl=[c['name']for c in w if c['continent']==a]
+    cl=[c for c in w if c['continent']==a]
     return render_template(
             'continent.html',
             length_of_cl=len(cl),
             cl=cl,
-            a=a
-            )
+             a=a)
     
 @app.route('/countryByName/<n>')
 def countryByNamePage(n):
@@ -55,5 +62,49 @@ def deleteCountry(n):
         page_size=page_size,
         page_number=0
         )
+
+@app.route('/editcountryByName/<n>')
+def editcountryByNamePage(n):
+    c=None
+    for x in w:
+        if x['name']==n:
+            c=x
+    return render_template(
+        'country-edit.html',
+        c=c)
+
+@app.route('/updatecountryByName')
+def updatecountryByNamePage():
+    n=request.args.get('country')
+    c=None
+    for x in w:
+        if x['name']==n:
+            c=x
+    c['capital']=request.args.get('capital')
+    c['continent']=request.args.get('continent')
+    return render_template(
+        'country.html',
+        c=c)
+
+@app.route('/createcountry')
+def createCountryByName():
+	return render_template('countrycreate.html', c=c)
+    
+@app.route('/addcountrybyname')
+def addcountryByNamePage():
+	n = request.args.get('country')
+	c = {}
+	c['name'] = n
+	c['capital'] = request.args.get('capital')
+	c['continent'] = request.args.get('continent')
+	c['area'] = int(request.args.get('area'))
+	c['gdp']  = float(request.args.get('gdp'))
+	c['tld']  = request.args.get('tld')
+	c['population']  = int(request.args.get('population'))
+	w.append(c)
+	w.sort(key = lambda c: c['name'])
+	return render_template(
+		'country.html',
+		c = c)
 
 app.run(host='0.0.0.0',port=8080,debug=True)
